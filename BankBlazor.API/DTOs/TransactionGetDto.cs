78c80d1f81d0a1f2a53dd03cf388a2ab8bfc88a8
siboo <siboo3@hotmail.com>
2026-05-14 07:57:40 +0200

    Add DTOs and refactor controllers to use DTOs

---
+++ BankBlazor.API/DTOs/TransactionGetDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BankBlazor.API.DTOs
+{
+    public class TransactionGetDto
+    {
+        public int AccountId { get; set; }
+        public DateOnly Date { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string Operation { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
