7b2bad89ee95e296e5a106cab5bba016e5c5b68e
siboo <siboo3@hotmail.com>
2026-05-14 04:27:49 +0200

    Added accountController, get account by Id and customr Id

---
+++ BankBlazor.API/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using BankBlazor.API.Data;
+using BankBlazor.API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankBlazor.API.Controllers
+{
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly BankBlazorContext _context;
+        public AccountController ( BankBlazorContext context )
+        {
+            _context = context;
+        }
+        //Get account by id
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<Account>> GetAccount ( int id )
+        {
+            var account = await _context.Accounts
+                .Include (a => a.Transactions)
+                .FirstOrDefaultAsync (a => a.AccountId == id);
+
+            if (account == null)
+                return NotFound ();
+
+            return account;
+        }
+
+        //Get: account by customer id
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Account>>> GetAccountsByCustomer ( int customerId )
+        {
+            var accounts = await _context.Dispositions
+                 .Where (d => d.CustomerId == customerId)
+                 .Select (d => d.Account)
+                 .ToListAsync ();
+            if (!accounts.Any ())
+            {
+                return NotFound ();
+            }
+            return Ok (accounts);
+        }
+    }
+}
