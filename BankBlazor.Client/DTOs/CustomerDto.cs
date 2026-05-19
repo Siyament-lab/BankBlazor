f809ffa00d42a2c54a7324340c74eb83f062e51d
siboo <siboo3@hotmail.com>
2026-05-19 10:37:30 +0200

    Added client DTOs for account, customers and transactionsGet

---
+++ BankBlazor.Client/DTOs/CustomerDto.cs
@@ -0,0 +1,17 @@
+﻿namespace BankBlazor.Client.DTOs
+{
+    public class CustomerDto
+    {
+        public int CustomerId { get; set; }
+        public string Givenname { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Emailaddress { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Streetaddress { get; set; } = string.Empty;
+        public string Zipcode { get; set; } = string.Empty;
+        public string Telephonenumber { get; set; } = string.Empty;
+        public DateOnly Birthday { get; set; }
+        public List<AccountDto> Accounts { get; set; } = new List<AccountDto> ();
+    }
+}
